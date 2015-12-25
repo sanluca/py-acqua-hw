d0427e5d8fe42472ec569fe7057ff11f72c77678
root <root@arietta.(none)>
2015-12-25 23:51:46 +0100

    aggiunto file di installazione calendar e wolf

---
+++ wolfcms/luxcal/luxcal430L-calendar/lcconfig.php
@@ -0,0 +1,10 @@
+<?php
+/*
+= LuxCal event calendar configuration =
+*/
+$lcV="4.3.0L"; //LuxCal version
+$dbType="SQLite"; //db type (MySQL or SQLite)
+$dbDir="db/"; //db directory
+$dbDef="mycal"; //default calendar (db name)
+$dbSel=1; //selectable in Options panel
+?>
