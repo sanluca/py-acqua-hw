e7fd5c41aaca685dfd84a211791993fd875512e5
luca <sanluca78@gmail.com>
2016-01-10 21:11:21 +0100

    cambio sistema in pwm.py

--- py/lib/pwm.py
+++ py/lib/pwm.py
@@ -23,32 +23,46 @@ class Mypwm():
     
     def pwm_export(self,n):
         
-        f = open('/sys/class/pwm/pwmchip0/export','w')
-        f.write(str(n))
-        f.close()
+        command="echo %s > /sys/class/pwm/pwmchip0/export" %notos.system(command)
+        
+        #f = open('/sys/class/pwm/pwmchip0/export','w')
+        #f.write(str(n))
+        #f.close()
         
     def pwm_unexport(self,n):
         
-        f = open('/sys/class/pwm/pwmchip0/unexport','w')
-        f.write(str(n))
-        f.close()
+        command=("echo %s > /sys/class/pwm/pwmchip0/unexport" %n)
+        os.system(command)
+        
+        #f = open('/sys/class/pwm/pwmchip0/unexport','w')
+        #f.write(str(n))
+        #f.close()
         
     def pwm_period(self,n,p):
         
-        f = open('/sys/class/pwm/pwmchip0/pwm%s/period' %str(n),'w')
-        f.write(str(p))
-        f.close()
+        command= "echo %s > /sys/class/pwm/pwmchip0/pwm%s/period" %(n,p)
+        os.system(command)
+        
+        #f = open('/sys/class/pwm/pwmchip0/pwm%s/period' %str(n),'w')
+        #f.write(str(p))
+        #f.close()
         
     def pwm_duty_cycle(self,n,p):
         
-        f = open('/sys/class/pwm/pwmchip0/pwm%s/duty_cycle' %str(n),'w')
-        f.write(str(p))
-        f.close()
+        command= "echo %s > /sys/class/pwm/pwmchip0/pwm%s/duty_cycle" %(n,p)
+        os.system(command)
+        
+        #f = open('/sys/class/pwm/pwmchip0/pwm%s/duty_cycle' %str(n),'w')
+        #f.write(str(p))
+        #f.close()
         
     def pwm_enable(self,n,p):
         
-        f = open('/sys/class/pwm/pwmchip0/pwm%s/enable' %str(n),'w')
-        f.write(str(p))
-        f.close()
+        command= "echo %s > /sys/class/pwm/pwmchip0/pwm%s/enable" %(n,p)
+        os.system(command)
+        
+        #f = open('/sys/class/pwm/pwmchip0/pwm%s/enable' %str(n),'w')
+        #f.write(str(p))
+        #f.close()
         
     
