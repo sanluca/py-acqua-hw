7e4a6646849b8f29bd073524980bb2138da8db46
luca <sanluca78@gmail.com>
2016-01-10 20:48:46 +0100

    a

--- py/lib/pwm.py
+++ py/lib/pwm.py
@@ -25,30 +25,30 @@ class Mypwm():
         
         f = open('/sys/class/pwm/pwmchip0/export','w')
         f.write(str(n))
-        #f.close()
+        f.close()
         
     def pwm_unexport(self,n):
         
         f = open('/sys/class/pwm/pwmchip0/unexport','w')
         f.write(str(n))
-        #f.close()
+        f.close()
         
     def pwm_period(self,n,p):
         
         f = open('/sys/class/pwm/pwmchip0/pwm%s/period' %str(n),'w')
         f.write(str(p))
-        #f.close()
+        f.close()
         
     def pwm_duty_cycle(self,n,p):
         
         f = open('/sys/class/pwm/pwmchip0/pwm%s/duty_cycle' %str(n),'w')
         f.write(str(p))
-        #f.close()
+        f.close()
         
     def pwm_enable(self,n,p):
         
         f = open('/sys/class/pwm/pwmchip0/pwm%s/enable' %str(n),'w')
         f.write(str(p))
-        #f.close()
+        f.close()
         
     
