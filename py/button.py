f72dc232f43343e1ca1422426b10de75a68f294c
luca <sanluca78@gmail.com>
2016-07-23 19:56:42 +0200

    non va il self

--- py/button.py
+++ py/button.py
@@ -54,8 +54,8 @@ class MyButton(Thread):
     def event_in2(self):
         self.db.update_real_time('status','in2',1)
         
-    in1.set_edge("both", self.event_in1)
-    in2.set_edge("both", self.event_in2)
+    in1.set_edge("both", event_in1)
+    in2.set_edge("both", event_in2)
 
     def run(self):
         if self.intro==True:
